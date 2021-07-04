6cc1ce7dd136b957897ebd08e3a2e0e117007a1c
Danil Nurgaliev <Danilkchik@gmail.com>
2021-07-04 14:30:33 +0300

    Setup project

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Danil Nurgaliev on 04.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
