b68745537be131086db969d2890ea1dfd88eafd8
Danil Nurgaliev <Danilkchik@gmail.com>
2021-07-04 14:56:38 +0300

    Completed first lesson

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -8,8 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var flipsCount = 0 {
+        didSet {
+            flipsCountLabel.text = "Flips: \(flipsCount)"
+        }
+    }
+
+    @IBOutlet weak var flipsCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    var emojiChoices = ["🐶", "🐻", "🐶", "🐻"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipsCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
+    }
+
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .red
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = .white
+        }
     }
 }
