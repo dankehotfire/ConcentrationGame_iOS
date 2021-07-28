04eee468a8fd1e2c82bdca700099df937822f449
Danil Nurgaliev <Danilkchik@gmail.com>
2021-07-28 15:49:45 +0300

    Update method for only one face up card

--- Concentration/Concentration/Model/Concentration.swift
+++ Concentration/Concentration/Model/Concentration.swift
@@ -26,17 +26,7 @@ class Concentration {
     
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
         }
         set {
             for index in cards.indices {
@@ -87,5 +77,11 @@ class Concentration {
             cards += [card, card]
         }
         cards.shuffle()
+    }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
